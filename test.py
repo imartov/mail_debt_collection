d23bbf6a8eadbd8d1a9b9506bf8c72f8186d9cdf
imartov <alexandr.kosyrew@mail.ru>
2023-07-09 01:16:02 +0300

    commit

--- test.py
+++ test.py
@@ -11,33 +11,67 @@ sender_mail = os.getenv('SENDER_MAIL')
 mail_sender_password = os.getenv('MAIL_SENDER_PASSWORD')
 recipient_mail = "alexandr.kosyrew@mail.ru"
 
-textfile = "text_message.txt"
-
 # Open the plain text file whose name is in textfile for reading.
-with open(textfile, "r", encoding="utf-8") as fp:
-    # Create a text/plain message
-    msg = EmailMessage()
-    msg.set_content('')
+msg = EmailMessage()
+msg.set_content('')
 
 # me == the sender's email address
 # you == the recipient's email addresss
-msg['Subject'] = f'The contents of {textfile}'
+msg['Subject'] = f'Тестирование email-рассылки'
 msg['From'] = sender_mail
 msg['To'] = recipient_mail
 
+# add styles to html-content
+with open('static\\styles.html', "r", encoding="utf-8") as file:
+    styles = file.read()
+    msg.add_alternative(styles, subtype='html')
+
+# add logo block to tml-content
 with open("static\\logo.html", "r", encoding="utf-8") as file:
     logo = file.read()
     
+# code for correct desplaying logo
 asparagus_cid = make_msgid()
 msg.add_alternative(logo.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
 
-with open("static\\corporate-logo.png", 'rb') as img:
+with open("static\\images\\corporate-logo.png", 'rb') as img:
     msg.get_payload()[1].add_related(img.read(), 'image', 'jpeg',
                                      cid=asparagus_cid)
     
+# add basic content with text to html-content
 with open("static\\email_template.html", "r", encoding="utf-8") as file:
     body = file.read()
     msg.add_alternative(body, subtype="html")
+
+asparagus_cid_inst = make_msgid()
+with open("static\\footer-inst.html", "r", encoding="utf-8") as file:
+    footer = file.read()
+
+msg.add_alternative(footer.format(instagram_icon=asparagus_cid_inst[1:-1]), subtype='html')
+
+with open("static\\images\\instagram-icon.png", 'rb') as img:
+    msg.get_payload()[1].add_related(img.read(), 'image', 'jpeg',
+                                     cid=asparagus_cid_inst)
+    
+asparagus_cid_link = make_msgid()
+with open("static\\footer-link.html", "r", encoding="utf-8") as file:
+    footer = file.read()
+
+msg.add_alternative(footer.format(linkedin_icon=asparagus_cid_link[1:-1]), subtype='html')
+
+with open("static\\images\\linkedin-icon.png", 'rb') as img:
+    msg.get_payload()[1].add_related(img.read(), 'image', 'jpeg',
+                                     cid=asparagus_cid_link)
+    
+asparagus_cid_site = make_msgid()
+with open("static\\footer-site.html", "r", encoding="utf-8") as file:
+    footer = file.read()
+
+msg.add_alternative(footer.format(site_icon=asparagus_cid_site[1:-1]), subtype='html')
+
+with open("static\\images\\site-icon.png", 'rb') as img:
+    msg.get_payload()[1].add_related(img.read(), 'image', 'jpeg',
+                                     cid=asparagus_cid_site)
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
